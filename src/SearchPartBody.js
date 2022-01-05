49c72ca4647d040361d30357a8626a705c7f3c98
unknown <c4h4kon4s@keio.jp>
2022-01-05 13:53:17 +0900

    soichiro joe role change

--- src/SearchPartBody.js
+++ src/SearchPartBody.js
@@ -4,12 +4,47 @@ import React from 'react';
 class SearchPartBody extends React.Component{
     constructor(props) {
         super(props);
+        this.state= {isSearchOpen: false}
+    }
+
+    handleOpenSearch() {
+
+        this.setState({ isSearchOpen : true} );
+
+    }
+
+    handleCloseSearch() {
+
+        this.setState({ isSearchOpen : false} );
+
     }
 
     render(){
+        
+        let searchBox;
+
+        if(this.state.isSearchOpen){
+            searchBox=( 
+                <div>
+                    <input 
+                        value = "文字を入力してください"
+                    />
+                    <button onClick= { ()=>this.handleCloseSearch() }>
+                        検索ボックスを閉じる
+                    </button>
+                </div>
+            );
+        }
         return(
             <div>
-                SearchPart DAYO
+                <button
+                    onClick={()=>{ this.handleOpenSearch()}}
+                >
+                    検索
+                </button>
+                <div>
+                    { searchBox }
+                </div>
             </div>
         );
     }
