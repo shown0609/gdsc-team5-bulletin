2aeec630ffa7fe4131d12b37f16d62ce611fa217
shown0609 <97076296+shown0609@users.noreply.github.com>
2022-01-07 00:44:27 +0900

    Update InputPartBody.js

--- src/InputPartBody.js
+++ src/InputPartBody.js
@@ -1,17 +1,50 @@
 import React from 'react';
+import PostedPartBody from './PostedPartBody';
 
+class InputPartBody extends React.Component {
+  handleClickPost(){
+    this.setState({isModalOpen: true});
+  }
 
-class InputPartBody extends React.Component{
-    constructor(props) {
-        super(props);
+  handleClickReply(){
+    this.setState({isModalOpen: false});
+  }
+
+  constructor(props){
+      super(props);
+      this.state={isModalOpen: false}
+  };
+
+  render(){
+    let modal;
+    if(this.state.isModalOpen){
+      modal = (
+        <div>
+          <div>
+            <App />
+          </div>
+          <button
+            className="closeReply"　
+            onClick={() =>{this.handleClickReply()}}
+          >
+            返信する
+          </button>
+        </div>
+      );
     }
 
-    render(){
-        return(
-            <div>
-                inputPart DAYO
-            </div>
-        );
-    }
-}
+    return(
+      <div>
+          <div className='modal2' onClick={() =>{this.handleClickPost()}}>
+            <button>返信する</button>
+          </div>
+          {modal}
+      </div>
+
+
+    )
+  }
+
+  }
+
 export default InputPartBody;
